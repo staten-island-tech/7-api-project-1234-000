8cfaa9520dc460c3090135bcdaa8ec946a2716ca
1234-000 <wilsonm68@nycstudents.net>
2026-05-07 21:28:52 -0400

    s

--- tester.py
+++ tester.py
@@ -7,7 +7,8 @@ def getFruit():
     url = requests.get(f"https://fruityvice.com/api/fruit/{text}")
     if url.status_code != 200:
         error.config(text = "none try again")
-        return None
+        return
+    error.config(text = "")
     data  = url.json()
     name_label.config(text = f"Name: {data["name"]}")
     family_label.config(text = f"Family: {data["family"]}")
@@ -18,27 +19,27 @@ def getFruit():
     carbslabel.config(f"Carbs: {data["nutritions"]["carb"]}")
 
 window = tk.Tk()
-window.title("Diet For You")
+window.title("Diet")
 window.geometry("1200x800")
 window.resizable(False,False)
 
-ask = tk.Label(window, text = "What Fruit Would You Like To Find About??", font = ("Arial", 14, "bold"), fg = "yellow")
+ask = tk.Label(window, text = "What Fruit Would You Like To Find About??", font = ("Arial", 50, "bold"), fg = "yellow")
 ask.pack()
 
-name_label = tk.Label(window, text = "", font = ("Arial", 14, "bold"))
+name_label = tk.Label(window, text = "", font = ("Arial", 25, "bold"))
 name_label.pack()
-family_label = tk.Label(window, text = "", font = ("Arial", 14, "bold"))
+family_label = tk.Label(window, text = "", font = ("Arial", 25, "bold"))
 family_label.pack()
-genus_label = tk.Label(window,text= "", font = ("Arial", 14, "bold"))
+genus_label = tk.Label(window,text= "", font = ("Arial", 25, "bold"))
 genus_label.pack()
-nutrition_label = tk.Label(window, text = "", font = ("Arial", 14, "bold"))
+nutrition_label = tk.Label(window, text = "", font = ("Arial", 25, "bold"))
 nutrition_label.pack()
 
-fat_label = tk.Label(window,text = "",font = ("Arial", 14, "bold"))
+fat_label = tk.Label(window,text = "",font = ("Arial", 25, "bold"))
 fat_label.pack()
-proteinlabel = tk.Label(window, text = "", font = ("Arial", 14, "bold"))
+proteinlabel = tk.Label(window, text = "", font = ("Arial", 25, "bold"))
 proteinlabel.pack()
-carbslabel = tk.Label(window, text = "", font = ("Arial", 14, "bold"))
+carbslabel = tk.Label(window, text = "", font = ("Arial", 25, "bold"))
 carbslabel.pack()
 
 error = tk.Label(window, text = "",font = ("Arial",50,"bold"))
